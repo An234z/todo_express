db2b8b0ea7caec8932e19067a43cd3850dccc2ee
Anni Voore <anni.voore@voco.ee>
2024-09-20 12:18:37 +0300

    Write data to file

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const express = require("express")
+const { isUtf8 } = require('buffer')
+const express = require('express')
 const app = express()
-const fs = require("fs");
+const port = 3002
+const fs = require('fs')
 
 
-const path = require("path")
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get("/", (req, res) => {
-
- fs.readFile("./tasks", "utf8", (err, data) => {
-    if (err) {
+const readFile = (filename) =>{
+  return new Promise((resolve, reject) => {
+    // get data form file
+    fs.readFile('./tasks', 'utf8', (err, data) =>{
+      if (err){
         console.error(err);
         return;
-    }
-    const tasks = data.split("\n")
-    res.render("index", {tasks: tasks})
+      }
+      // tasks list data from file
+      const tasks = data.split("\n")
+      resolve(tasks)
     });
+  })
+} 
+
+app.get('/', (req, res) => {
+  // tasks list data from file
+  readFile('./tasks')
+    .then(tasks =>{
+    console.log(tasks)
+    res.render('index',{tasks: tasks})
+  })
+ }) 
+ // For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  // tasks list data from file
+  readFile('./tasks')
+    .then(tasks =>{
+    // add from sent task to tasks array
+    tasks.push(req.body.task)
+    const data = tasks.join("\n")
+    fs.writeFile('./tasks', data, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.redirect('/')
+    })
+    })
+
 })
 
-app.listen(3001, () => {
-    console.log("Example app is started at http://localhost:3001")
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
 })
