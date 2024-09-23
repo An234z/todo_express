1f32bcf311c89ed0f93ae1ee842d8c2bb58c5062
Anni Voore <anni.voore@voco.ee>
2024-09-23 11:31:18 +0300

    Delete task data from file

--- index.js
+++ index.js
@@ -12,21 +12,32 @@ app.set('views', path.join(__dirname, 'views'))
 const readFile = (filename) =>{
   return new Promise((resolve, reject) => {
     // get data form file
-    fs.readFile('./tasks', 'utf8', (err, data) =>{
+    fs.readFile('./tasks.json', 'utf8', (err, data) =>{
       if (err){
         console.error(err);
         return;
       }
       // tasks list data from file
-      const tasks = data.split("\n")
+      const tasks = JSON.parse(data)
       resolve(tasks)
     });
   })
 } 
+const writeFile = (filename, data) => {
+ return new Promise((resolve, reject) => {
+  fs.writeFile(filename, data, "utf-8", err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    resolve(true)
+  });
+})
+}
 
 app.get('/', (req, res) => {
   // tasks list data from file
-  readFile('./tasks')
+  readFile('./tasks.json')
     .then(tasks =>{
     console.log(tasks)
     res.render('index',{tasks: tasks})
@@ -37,21 +48,44 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
   // tasks list data from file
-  readFile('./tasks')
+  readFile('./tasks.')
     .then(tasks =>{
+
+      let index
+      if(tasks.length === 0)
+      {
+        index = 0
+      } else { 
+        index = tasks[tasks.length -1].id + 1; 
+    }  
+
+    const newTask = {
+        "id" : index,
+        "task" : req.body.task
+    }
     // add from sent task to tasks array
-    tasks.push(req.body.task)
-    const data = tasks.join("\n")
-    fs.writeFile('./tasks', data, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+    tasks.push(newTask)
+    data = JSON.stringify(tasks, null, 2)
+    writeFile("tasks.json", data)
       res.redirect('/')
     })
-    })
+ })
 
+app.get("/delete-task/:taskId", (req, res) => {
+let deletedTaskId = parseInt(req.params.taskId)
+readFile("./tasks.json")
+.then(tasks=> {
+  tasks.forEach((task, index) => {
+    if(task.id === deletedTaskId){
+    tasks.splice(index, 1)
+  }
 })
+data = JSON.stringify(tasks, null, 2)
+writeFile("tasks.json", data)
+   res.redirect("/")
+  })
+ })
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
